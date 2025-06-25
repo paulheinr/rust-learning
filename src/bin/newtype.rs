e727ca96401fb7f2b054316694f1f48f264c5fb0
paulh <heinrich@vsp.tu-berlin.de>
2025-06-25 23:45:09 +0200

    solved with generics

--- src/bin/newtype.rs
+++ src/bin/newtype.rs
@@ -1,9 +1,10 @@
 use std::hash::{DefaultHasher, Hash, Hasher};
+use std::marker::PhantomData;
 use thiserror::Error;
 
 /// implements the tasks from https://www.howtocodeit.com/articles/ultimate-guide-rust-newtypes
 fn main() {
-    let password = Password::new(String::from("ThisIsAPassword")).unwrap();
+    let password = Password::<DefaultPasswordPolicy>::new(String::from("aa")).unwrap();
 
     match password.check("ThisIsAPassword1") {
         Ok(_) => println!("Password matches!"),
@@ -11,7 +12,34 @@ fn main() {
     }
 }
 
-struct Password(u64);
+trait PasswordPolicy {
+    fn check(candidate: &str) -> Result<(), PasswordCreationError>;
+}
+
+struct DefaultPasswordPolicy;
+
+impl PasswordPolicy for DefaultPasswordPolicy {
+    fn check(candidate: &str) -> Result<(), PasswordCreationError> {
+        if candidate.len() < 8 {
+            return Err(PasswordCreationError(candidate.to_string()));
+        }
+        if !candidate.is_ascii() {
+            return Err(PasswordCreationError(candidate.to_string()));
+        }
+        Ok(())
+    }
+}
+
+struct NoPasswordPolicy;
+
+impl PasswordPolicy for NoPasswordPolicy {
+    fn check(_candidate: &str) -> Result<(), PasswordCreationError> {
+        Ok(())
+    }
+}
+
+// Not sure whether static generics are the right way to go here, but it works
+struct Password<P: PasswordPolicy>(u64, PhantomData<P>);
 
 #[derive(Debug, Error, Clone, PartialEq)]
 #[error("{0} is not a valid password")]
@@ -21,17 +49,13 @@ struct PasswordCreationError(String);
 #[error("{0} does not match the password")]
 struct PasswordCheckError(String);
 
-impl Password {
-    fn new(password: String) -> Result<Password, PasswordCreationError> {
-        if password.len() < 8 {
-            Err(PasswordCreationError(password))
-        } else if !password.is_ascii() {
-            Err(PasswordCreationError(password))
-        } else {
-            let mut hasher = DefaultHasher::default();
-            password.hash(&mut hasher);
-            Ok(Password(hasher.finish()))
-        }
+impl<P: PasswordPolicy> Password<P> {
+    fn new(password: String) -> Result<Password<P>, PasswordCreationError> {
+        P::check(password.as_str())?;
+        
+        let mut hasher = DefaultHasher::default();
+        password.hash(&mut hasher);
+        Ok(Password(hasher.finish(), PhantomData))
     }
     
     pub fn check(&self, candidate: &str) -> Result<(), PasswordCheckError> {
